112fb1f0a1fac57a8cf40953fdf089594e56143a
Anneliese Doerfler <anneliesedoerfler@gmail.com>
2020-04-14 15:43:20 -0700

    Pacman animation

---
+++ PacMan/Assets/Scripts/PacmanMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanMovement : MonoBehaviour {
+	public float speed = 0.4f;
+	Vector2 dest = Vector2.zero;
+	// Use this for initialization
+	void Start () {
+		dest = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+        GetComponent<Rigidbody2D>().MovePosition(p);
+		if ((Vector2)transform.position == dest) {
+        	if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
+            dest = (Vector2)transform.position + Vector2.up;
+        	if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
+            dest = (Vector2)transform.position + Vector2.right;
+        	if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
+            dest = (Vector2)transform.position - Vector2.up;
+        	if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
+            dest = (Vector2)transform.position - Vector2.right;
+		}
+	}
+	bool valid(Vector2 dir){
+		Vector2 pos = transform.position;
+		RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+		return (hit.collider == GetComponent<Collider2D>());
+	}
+}
