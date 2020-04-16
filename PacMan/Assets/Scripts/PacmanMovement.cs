1af2431cc98e342ff46647dce78e013bf012e59e
Anneliese Doerfler <anneliesedoerfler@gmail.com>
2020-04-15 20:10:30 -0700

    pacman movement

--- PacMan/Assets/Scripts/PacmanMovement.cs
+++ PacMan/Assets/Scripts/PacmanMovement.cs
@@ -1,33 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class PacmanMovement : MonoBehaviour {
-	public float speed = 0.4f;
-	Vector2 dest = Vector2.zero;
-	// Use this for initialization
-	void Start () {
-		dest = transform.position;
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+    public float speed = 0.4f;
+    Vector2 dest = Vector2.zero;
+
+    void Start() {
+        dest = transform.position;
+    }
+
+    void FixedUpdate() {
+        // Move closer to Destination
+        Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
         GetComponent<Rigidbody2D>().MovePosition(p);
-		if ((Vector2)transform.position == dest) {
-        	if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
-            dest = (Vector2)transform.position + Vector2.up;
-        	if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
-            dest = (Vector2)transform.position + Vector2.right;
-        	if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
-            dest = (Vector2)transform.position - Vector2.up;
-        	if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
-            dest = (Vector2)transform.position - Vector2.right;
-		}
-	}
-	bool valid(Vector2 dir){
-		Vector2 pos = transform.position;
-		RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
-		return (hit.collider == GetComponent<Collider2D>());
-	}
+
+        // Check for Input if not moving
+        if ((Vector2)transform.position == dest) {
+            if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
+                dest = (Vector2)transform.position + Vector2.up;
+            if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
+                dest = (Vector2)transform.position + Vector2.right;
+            if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
+                dest = (Vector2)transform.position - Vector2.up;
+            if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
+                dest = (Vector2)transform.position - Vector2.right;
+        }
+        Vector2 dir = dest - (Vector2)transform.position;
+        GetComponent<Animator>().SetFloat("DirX", dir.x);
+        GetComponent<Animator>().SetFloat("DirY", dir.y);
+    }
+
+    bool valid(Vector2 direction) {
+        Vector2 pos = transform.position;
+        direction = direction * 0.5f;
+        RaycastHit2D hit = Physics2D.Linecast(pos + direction, pos);
+        return (hit.collider == GetComponent<Collider2D>());
+    }
 }
