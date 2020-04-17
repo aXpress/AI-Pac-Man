0b56ea26e909558ba0c3650274f30729ee3eb609
markwelc <markwelc@oregonstate.edu>
2020-04-17 14:13:47 -0700

    added dots and froze Pacman's rotation

---
+++ PacMan/Assets/Scripts/dotBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dotBehavior : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject.name == "pacman_sprite_0")
+			Destroy(this.gameObject);
+			// Debug.Log("collsion detected");
+	}
+}
