798c3ad019fb5eb7965e89dd5f47a240a2f5ef42
Will Geller <wgeller110@gmail.com>
2020-04-17 16:39:54 -0700

    ghost checkpoints
    code uses checkpoints for ghost

---
+++ PacMan/Assets/ghostMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ghostMovement : MonoBehaviour {
+	public Transform [] checkpoints;
+	private int cur = 0;
+	public float ghostSpeed = .01f;
+
+	void FixedUpdate () 
+	{
+		if(transform.position != checkpoints[cur].position)
+		{
+			Vector2 pos = Vector2.MoveTowards(transform.position, checkpoints[cur].position, ghostSpeed);
+			GetComponent<Rigidbody2D>().MovePosition(pos);
+		}
+		else 
+		{
+			cur = cur + 1 % (checkpoints.Length);
+		}
+	}
+}
