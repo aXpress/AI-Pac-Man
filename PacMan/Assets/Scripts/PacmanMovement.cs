6c46a9ee830de06b81a37a60b291684023504af0
markwelc <markwelc@oregonstate.edu>
2020-04-17 16:49:53 -0700

    fixed pacman getting stuck on walls

--- PacMan/Assets/Scripts/PacmanMovement.cs
+++ PacMan/Assets/Scripts/PacmanMovement.cs
@@ -14,17 +14,15 @@ public class PacmanMovement : MonoBehaviour {
         Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
         GetComponent<Rigidbody2D>().MovePosition(p);
 
-        // Check for Input if not moving
-        if ((Vector2)transform.position == dest) {
-            if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
-                dest = (Vector2)transform.position + Vector2.up;
-            if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
-                dest = (Vector2)transform.position + Vector2.right;
-            if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
-                dest = (Vector2)transform.position - Vector2.up;
-            if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
-                dest = (Vector2)transform.position - Vector2.right;
-        }
+        // Check for Input if not moving or if moving
+        if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
+            dest = (Vector2)transform.position + Vector2.up;
+        if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
+            dest = (Vector2)transform.position + Vector2.right;
+        if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
+            dest = (Vector2)transform.position - Vector2.up;
+        if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
+            dest = (Vector2)transform.position - Vector2.right;
         Vector2 dir = dest - (Vector2)transform.position;
         GetComponent<Animator>().SetFloat("DirX", dir.x);
         GetComponent<Animator>().SetFloat("DirY", dir.y);
