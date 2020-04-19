60d5a6af9ebb0e606e3e4cb2ded91371540c23f9
Anneliese Doerfler <anneliesedoerfler@gmail.com>
2020-04-18 23:16:57 -0700

    decrementing lives Please enter the commit message for your changes. Lines starting

--- PacMan/Assets/Scripts/PacmanMovement.cs
+++ PacMan/Assets/Scripts/PacmanMovement.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class PacmanMovement : MonoBehaviour {
     public float speed = 4.0f;
     Vector2 dest = Vector2.zero;
+    
+    int lives = 3;
 
     void Start() {
         //dest = transform.position;
@@ -33,4 +35,30 @@ public class PacmanMovement : MonoBehaviour {
         GetComponent<Animator>().SetFloat("DirY", dest.y);
         transform.localPosition += (Vector3)(dest * speed) * Time.deltaTime;
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+	{
+        bool hit_ghost = false;
+		if(collision.gameObject.name == "blue_ghost")
+            hit_ghost = true;
+        if(collision.gameObject.name == "pink_ghost")
+            hit_ghost = true;
+        if(collision.gameObject.name == "red_ghost")
+            hit_ghost = true;
+        if(collision.gameObject.name == "orange_ghost")
+            hit_ghost = true;
+        if(hit_ghost == true){
+            if(lives == 3){
+                Destroy (GameObject.FindWithTag("life_three"));
+            }
+            else if(lives == 2){
+                Destroy (GameObject.FindWithTag("life_two"));
+            }
+            else if(lives == 1){
+                Destroy (GameObject.FindWithTag("life_one"));
+            }
+            lives--;
+            //Debug.Log(lives);
+        }
+	}
 }
