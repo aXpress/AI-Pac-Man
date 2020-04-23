c99d0d9a92d5f4e4524c7a7152e76a505bfd9772
Will Geller <wgeller110@gmail.com>
2020-04-23 11:07:52 -0700

    Ghost script has wait time
    added wait period to ghost script

--- PacMan/Assets/Scripts/ghostMovement.cs
+++ PacMan/Assets/Scripts/ghostMovement.cs
@@ -3,20 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ghostMovement : MonoBehaviour {
+	// Ghost active variables
+	private bool active = false;
+	public int waitTime;
+
+	// Ghost movement variables
+	public float ghostSpeed;
 	public Transform [] checkpoints;
 	private int cur = 0;
-	public float ghostSpeed = .01f;
 
 	void FixedUpdate () 
 	{
-		if(transform.position != checkpoints[cur].position)
+		if (!active && (Time.unscaledTime >= waitTime))
 		{
-			Vector2 pos = Vector2.MoveTowards(transform.position, checkpoints[cur].position, ghostSpeed);
-			GetComponent<Rigidbody2D>().MovePosition(pos);
+			active = true;
 		}
-		else 
+		
+		if (active)
 		{
-			cur = (cur + 1) % checkpoints.Length;
+			if (transform.position != checkpoints[cur].position)
+			{
+				Vector2 pos = Vector2.MoveTowards(transform.position, checkpoints[cur].position, ghostSpeed);
+				GetComponent<Rigidbody2D>().MovePosition(pos);
+			}
+			else
+			{
+				cur = (cur + 1) % checkpoints.Length;
+			}
 		}
+		
 	}
 }
