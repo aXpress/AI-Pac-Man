b8fc4102553d593d89dda9096e89e21f283ff3fc
Anneliese Doerfler <anneliesedoerfler@gmail.com>
2020-04-29 15:36:56 -0700

    Respawning and Gameover for Player

--- PacMan/Assets/Scripts/PacmanMovement.cs
+++ PacMan/Assets/Scripts/PacmanMovement.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Threading;
+using UnityEngine.SceneManagement;
 
 public class PacmanMovement : MonoBehaviour {
     public float speed = 4.0f;
-    Vector2 dest = Vector2.zero;
+    Vector3 dest = Vector2.zero;
+    Vector3 originalPos;
     
     int lives = 3;
 
     void Start() {
-        //dest = transform.position;
+        originalPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
     }
 
     void Update() {
@@ -18,16 +20,16 @@ public class PacmanMovement : MonoBehaviour {
     }
     void checkInput(){
         if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
-            dest = Vector2.up;
+            dest = Vector3.up;
         if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
-            dest = Vector2.right;
+            dest = Vector3.right;
         if (Input.GetKey(KeyCode.DownArrow) && valid(Vector2.down))
-            dest = Vector2.down;
+            dest = Vector3.down;
         if (Input.GetKey(KeyCode.LeftArrow) && valid(Vector2.left))
-            dest = Vector2.left;
+            dest = Vector3.left;
     }
-    bool valid(Vector2 direction) {
-        Vector2 pos = transform.position;
+    bool valid(Vector3 direction) {
+        Vector3 pos = transform.position;
         RaycastHit2D hit = Physics2D.Linecast(pos + direction, pos);
         return (hit.collider == GetComponent<Collider2D>());
     }
@@ -57,11 +59,16 @@ public class PacmanMovement : MonoBehaviour {
             }
             else if(lives == 1){
                 Destroy (GameObject.FindWithTag("life_one"));
+                SceneManager.LoadScene("resultsScene");
+                return;
             }
             lives--;
-            int milliseconds = 500;
+            int milliseconds = 1000;
             Thread.Sleep(milliseconds);
-            //Debug.Log(lives);
+            gameObject.transform.position = originalPos;
+            GetComponent<Animator>().SetFloat("DirX", dest.x);
+            GetComponent<Animator>().SetFloat("DirY", dest.y);
+            Thread.Sleep(milliseconds);
         }
 	}
 }
