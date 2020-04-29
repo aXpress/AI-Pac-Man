442b729ddc8e4acf04c8bbb76e4b0737cd00f5f0
aXpress <chengabraham@yahoo.com>
2020-04-29 14:57:30 -0700

    Add menu navigation and UI
    Add buttons to navigate through scenes
    Add rules page UI
    Add results page UI

---
+++ PacMan/Assets/results_screen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class results_screen : MonoBehaviour {
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("pacmanScene");
+    }
+
+    public void GoToMain()
+    {
+        SceneManager.LoadScene("menuScene");
+    }
+}
