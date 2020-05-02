d7eda27e58bad4250d7b440acdd9ba552d73aaa1
Will Geller <wgeller110@gmail.com>
2020-05-01 17:51:57 -0700

    Revert "Added player and ai score"
    This reverts commit efda33fdb6db1b42ddae823e9062b0066a90ad0b.

--- PacMan/Assets/Scripts/playerScore.cs
+++ PacMan/Assets/Scripts/playerScore.cs
@@ -2,21 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 using System;
 public class playerScore : MonoBehaviour {
 
-	public static int scoreValue = 0;
-	TextMeshPro score;
-
+	public static int score_val;
+	public Text score_text;
 	// Use this for initialization
 	void Start () {
-		score = GetComponent<TextMeshPro> ();
+		score_text = GetComponent<Text>();
+		score_val = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		score.text = "SCORE: " + scoreValue;
+		score_text.text = "" + score_val;
 
 	}
 }
