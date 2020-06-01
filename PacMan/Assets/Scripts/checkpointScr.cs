5d81b337faa44d0abb82efdaaa275a59463769b2
markwelc <markwelc@oregonstate.edu>
2020-06-01 15:29:52 -0700

    ghosts move randomly

---
+++ PacMan/Assets/Scripts/checkpointScr.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkpointScr : MonoBehaviour {
+
+	public bool up;
+	public bool down;
+	public bool left;
+	public bool right;
+
+	public bool start;
+}
--- PacMan/Assets/Scripts/ghostMovement.cs
+++ PacMan/Assets/Scripts/ghostMovement.cs
@@ -8,15 +8,20 @@ using UnityEngine;
 public class ghostMovement : MonoBehaviour {
 
 	public bool active = false;
-	public int cur = 0;
+	//public int cur = 0;
 	public Vector3 startPosition;
 	public float waitTime;
 	public float ghostSpeed;
-	public Transform [] checkpoints;
+	// public Transform [] checkpoints;
+	public Transform startTransform;
+	private bool firstMove;
+	private bool moveToCurTarget;
+	private Transform curTarget;
 
 	void Start ()
 	{
 		startPosition = gameObject.transform.position;
+		firstMove = true;
 	}
 
 	void FixedUpdate ()
@@ -29,16 +34,30 @@ public class ghostMovement : MonoBehaviour {
 		if (active)//only move if this is the script that should control the motion
 		{
 			// Ghost continues moving to destination/checkpoint
-			if (transform.position != checkpoints[cur].position)
+			// if (transform.position != checkpoints[cur].position)
+			// {
+			// 	Vector2 pos = Vector2.MoveTowards(transform.position, checkpoints[cur].position, ghostSpeed);
+			// 	GetComponent<Rigidbody2D>().MovePosition(pos);
+			// }
+			// // Ghost has reached destination and will move to next destination/checkpoint
+			// else
+			// {
+			// 	cur = (cur + 1) % checkpoints.Length;
+			// }
+
+			if(firstMove)
+				doFirstMove();
+
+			if(moveToCurTarget)
 			{
-				Vector2 pos = Vector2.MoveTowards(transform.position, checkpoints[cur].position, ghostSpeed);
-				GetComponent<Rigidbody2D>().MovePosition(pos);
+				moveToSpot(curTarget);
+				if(transform.position == curTarget.position)
+				{
+					chooseDirection(curTarget);
+					moveToCurTarget = false;
+				}
 			}
-			// Ghost has reached destination and will move to next destination/checkpoint
-			else
-			{
-				cur = (cur + 1) % checkpoints.Length;
-			}
+				
 		}
 	
 	}
@@ -47,14 +66,13 @@ public class ghostMovement : MonoBehaviour {
 	void ghostWaitTime(int stdWaitTime)
 	{
 		waitTime = stdWaitTime + Time.timeSinceLevelLoad;
-		active = false;
 	}
 
 
 	public void ghostRespawn(string name)
 	{
 		gameObject.transform.position = startPosition;
-		cur = 0;
+		//cur = 0;
 
 		if (name == "pink_ghost_p")
 			ghostWaitTime(3);
@@ -62,5 +80,79 @@ public class ghostMovement : MonoBehaviour {
 			ghostWaitTime(5);
 		else if (name == "orange_ghost_p")
 			ghostWaitTime(7);
+
+		active = false;
+		firstMove = true;
 	}
+
+
+	void doFirstMove()
+	{
+
+		moveToSpot(startTransform);
+
+		if(transform.position == startTransform.position)
+		{
+			firstMove = false;
+			if(Random.Range(0,2) == 1)
+				GetComponent<Rigidbody2D>().velocity = Vector2.left * ghostSpeed;
+			else
+				GetComponent<Rigidbody2D>().velocity = Vector2.right * ghostSpeed;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D col)
+	{
+		moveToCurTarget = true;
+		curTarget = col.gameObject.GetComponent<Transform>();
+	}
+
+
+	private void moveToSpot(Transform target)
+	{
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+		Vector2 pos = Vector2.MoveTowards(transform.position, target.position, 0.1f);
+		rb.MovePosition(pos);
+	}
+
+
+	private void chooseDirection(Transform col)
+	{
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+		if(col.GetComponent<Collider2D>().tag == "checkpoint")
+		{
+
+			rb.velocity = Vector2.zero;
+
+			//Debug.Log("collided with checkpoint");
+			bool [] directions = {false, false, false, false};
+			directions[0] = col.gameObject.GetComponent<checkpointScr>().up;
+			directions[1] = col.gameObject.GetComponent<checkpointScr>().down;
+			directions[2] = col.gameObject.GetComponent<checkpointScr>().left;
+			directions[3] = col.gameObject.GetComponent<checkpointScr>().right;
+
+			int direc = Random.Range(0,4);
+			while(!directions[direc])
+				direc = Random.Range(0,4);
+
+			switch(direc)
+			{
+				case 0: // move up
+					rb.velocity = Vector2.up * ghostSpeed;
+					break;
+				case 1: // move down
+					rb.velocity = Vector2.down * ghostSpeed;
+					break;
+				case 2: // move left
+					rb.velocity = Vector2.left * ghostSpeed;
+					break;
+				case 3: // move right
+					rb.velocity = Vector2.right * ghostSpeed;
+					break;
+			}
+		}
+	}
+
+
 }
