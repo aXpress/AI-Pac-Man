79497a078a880457819cba26d02bb53391a97085
markwelc <markwelc@oregonstate.edu>
2020-06-02 11:21:01 -0700

    made some bugs less noticeable

---
+++ PacMan/Assets/ghostMirror.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ghostMirror : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = new Vector2(142.8571f, 0);
+	}
+}
